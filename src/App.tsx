51b58a3eb9ccc6361caf6fb57182056c83b08f12
NaanArmaX <tainan56.tss@gmail.com>
2025-07-24 15:16:55 -0300

    Implementa Alterações no App (Rota base /)

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+const basename = import.meta.env.BASE_URL; 
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
